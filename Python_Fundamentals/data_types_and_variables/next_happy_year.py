78e7003cd3ac0c3b04f089315aeba45bc408bd61
Владимир Иванов <mechiamam@gmail.com>
2022-09-21 23:22:23 +0300

    Finished the last exercise from this group.

--- Python_Fundamentals/data_types_and_variables/next_happy_year.py
+++ Python_Fundamentals/data_types_and_variables/next_happy_year.py
@@ -1,22 +1,12 @@
-year = int(input("Enter your chosen year: "))
-
-
-def is_ascending(lst):
-    return sorted(lst) == lst
-
-
-distinct_digits = False
-while not distinct_digits:
-    if year_list[0] != year_list[1] and year_list[0] != year_list[4] and year_list[0] != year_list[3] \
-            and year_list[0] != year_list[2]:
-        distinct_digits = True
-        print(year_list)
-        break
+year = int(input())
+happy_year_condition = False
+while not happy_year_condition:
     year += 1
-    year_list = [int(year) for i in str(year)]
+    set_year = set()
+    for i in range(len(str(year))):
+        set_year.add(str(year)[i])
+    happy_year_condition = len(set_year) == len(str(year))
 
-# You are saying goodbye to your best friend:
-# "See you next happy year". Happy Year is the year with only distinct digits,
-# for example, 2018. Write a program that receives an integer number and finds the next happy year.
+print(year)
 
-# TODO
+
